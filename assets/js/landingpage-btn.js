e3af6304f13dbf142fe107ec64129f3e74bc06cc
Ming-doan <quangminh57dng@gmail.com>
2021-12-10 22:26:04 +0700

    Init

---
+++ assets/js/landingpage-btn.js
@@ -0,0 +1,31 @@
+const createRoom = document.querySelector('.create')
+const joinRoom = document.querySelector('.join')
+const randomRoom = document.querySelector('.ramdom')
+const container = document.querySelector('.container')
+const overlay = document.querySelector('.overlay')
+const guide = document.querySelector('.container.guide-overlay')
+const createInput = document.querySelector('.container.room-input-overlay')
+const helpBtn = document.querySelector('.help')
+const rankBtn = document.querySelector('.rank')
+const closeBtn = document.querySelector('.guide i')
+
+joinRoom.addEventListener('click',function() {
+    createInput.classList.remove('hidden')
+})
+
+overlay.addEventListener('click', function() {
+    createInput.classList.add('hidden')
+    guide.classList.add('hidden')
+})
+
+helpBtn.addEventListener('click', function() {
+    guide.classList.remove('hidden')
+})
+
+closeBtn.addEventListener('click', function() {
+    guide.classList.add('hidden')
+})
+
+randomRoom.addEventListener('click', function() {
+    window.location.href('')
+})
